890c113ffb625be740a577d4a5adb99146f3a0c9
sur0g <sur0gg@gmail.com>
2019-02-01 16:36:43 +0200

    ability to switch logging on/off

--- log.py
+++ log.py
@@ -1,4 +1,8 @@
 # My personal invention
 # TODO: add license
+import config
+
+
 def logi(message, description):
-    print("(I)", message.chat.id, description)
+    if config.log if hasattr(config, 'log') else True:
+        print("(I)", message.from_user.username, description, ("(id: {0})".format(message.chat.id)))
