f724d7a61adcd897776f819bd3302de053c0e729
sur0g <sur0gg@gmail.com>
2019-02-01 19:13:13 +0200

    add ability to rename session

--- main.py
+++ main.py
@@ -95,5 +95,49 @@ def delete_session(message):
     else:
         bot.reply_to(message, "Failed to delete: you probably don't have any here")
 
+
+@bot.message_handler(commands=['rename_session'])
+@authorise
+@private
+@logging
+def rename_session(message):
+    user = User.get_by_id(message.from_user.id)
+    sessions = Session.filter(curator=user)
+    session_mapping = {s.sessionId: s.name for s in sessions}
+
+    reply_text = '\n'.join([str(k) + ': ' + v for k, v in session_mapping.items()])
+    reply_text = 'Please chose one\n' + reply_text
+    session_select = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=len(sessions))
+    session_select.add(*[str(k) for k in session_mapping.keys()])
+    session_select.add('Cancel')
+    msg = bot.reply_to(message, reply_text, reply_markup=session_select)
+    bot.register_next_step_handler(msg, process_session_choose)
+
+
+chosen_sessions = {}
+
+
+def process_session_choose(message):
+    if message.text == 'Cancel':
+        bot.reply_to(message, 'Okay.jpg', reply_markup=hideBoard)
+        return
+    session = Session.get_by_id(message.text)
+    chosen_sessions[message.from_user.id] = session
+
+    msg = bot.reply_to(message, 'Define a new name', reply_markup=hideBoard)
+    bot.register_next_step_handler(msg, process_name_typing)
+
+
+def process_name_typing(m):
+    name = m.text
+    session: Session = chosen_sessions.get(m.from_user.id)
+    if session:
+        session.name = name
+        session.save()
+        bot.reply_to(m, 'Success')
+    else:
+        bot.reply_to(m, 'Couldn\'t find any sessions!')
+
+
 print("Starting the bot")
 bot.polling()
