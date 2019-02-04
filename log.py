6bb3e09e399693441868e5f830b2ce88cb68f6d5
barmaley <w.malygin@gmail.com>
2019-02-04 12:19:41 +0300

    use logging instead of logi

--- log.py
+++
@@ -1,8 +0,0 @@
-# My personal invention
-# TODO: add license
-import config
-
-
-def logi(message, description):
-    if config.log if hasattr(config, 'log') else True:
-        print("(I)", message.from_user.username, description, ("(id: {0})".format(message.chat.id)))
